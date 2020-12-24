25a6ee41f284685563ca2a37931505ed4df098fe
ikhsan-fauji <ikhsanfauji.dev@gmail.com>
2020-12-24 22:25:53 +0700

    detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,23 @@ class MyApp extends StatelessWidget {
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 16.0),
+            child: Text(
+              'Farm House Lembang',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
